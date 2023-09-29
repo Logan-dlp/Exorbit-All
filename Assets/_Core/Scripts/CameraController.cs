4dd42a8049a72236abf8fd073c849945f55fa1bb
Logan <pix.59700@gmail.com>
2023-09-29 14:43:59 +0200

    Add smooth camera ✅

---
+++ Assets/_Core/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform crosshaireObjectPosition;
+    [SerializeField] private float cameraStartRotationX;
+    [SerializeField] private float cameraStartRotationY;
+    [SerializeField] private float cameraClampRotation;
+    [SerializeField] private float smoothRotationDelay;
+
+    private void Update()
+    {
+        CameraSmoothRotation(crosshaireObjectPosition, cameraStartRotationX, cameraStartRotationY, cameraClampRotation, smoothRotationDelay);
+    }
+
+    private void CameraSmoothRotation(Transform _targetObject, float _startRotationX, float _startRotationY,float _clampRotation, float _delayRotation)
+    {
+        float _rotationX = _targetObject.position.y;
+        float _rotationY = _targetObject.position.x;
+        
+        Debug.Log(_rotationX);
+
+        _rotationX = Mathf.Clamp(_rotationX, -_clampRotation + _startRotationX, _clampRotation + _startRotationX);
+        _rotationY = Mathf.Clamp(_rotationY, -_clampRotation - _startRotationY, _clampRotation + _startRotationY);
+        
+        Debug.Log(_rotationX);
+        
+        Quaternion _target = Quaternion.Euler(-_rotationX * 100, _rotationY * 10, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, _target, Time.deltaTime * _delayRotation);
+    }
+}
--- Assets/_Core/Scripts/PlayerController.cs
+++ Assets/_Core/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(PlayerInput)), RequireComponent(typeof(CameraController))]
 public class PlayerController : MonoBehaviour
 {
     /*
